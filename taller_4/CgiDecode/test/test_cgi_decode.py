2344fcbd1f16832d88a807721c7f4421a0cbdc2e
mybelgo <mybelgo@gmail.com>
2024-04-18 19:12:44 -0300

    feat(taller 4): ejercicio 1

--- taller_4/CgiDecode/test/test_cgi_decode.py
+++ taller_4/CgiDecode/test/test_cgi_decode.py
@@ -4,9 +4,17 @@ from src.cgi_decode import cgi_decode
 
 
 class TestCgiDecode(unittest.TestCase):
-    def testExample(self):
-        # COMPLETAR
-        cgi_decode("Hello World")
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(True, False)
+    def testEmptyString(self):
+        self.assertEqual(cgi_decode(""), "")
+    
+    def testSpaces(self):
+        self.assertEqual(cgi_decode("Hello+World"), "Hello World")
+    
+    def testHexValues(self):
+        self.assertEqual(cgi_decode('%0e'), chr(14))
+
+    def testWrongFirstDigit(self):
+        self.assertRaises(ValueError, cgi_decode, '%g0')
+    
+    def testWrongSecondDigit(self):
+        self.assertRaises(ValueError, cgi_decode, '%0g')
