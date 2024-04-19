3e849b78b3d2017c2f224e6a5b6ca93a610f2dd1
mybelgo <mybelgo@gmail.com>
2024-04-18 21:18:50 -0300

    feat(taller 4): ejercicio 2

--- taller_4/CgiDecode/src/evaluate_condition.py
+++ taller_4/CgiDecode/src/evaluate_condition.py
@@ -4,20 +4,19 @@ from typing import Dict
 # Inicializar mappings globales
 distances_true: Dict[int, int] = {}
 distances_false: Dict[int, int] = {}
+K = 1
 
 
 def update_maps(condition_num, d_true, d_false):
     global distances_true, distances_false
 
     if condition_num in distances_true.keys():
-        distances_true[condition_num] = min(
-            distances_true[condition_num], d_true)
+        distances_true[condition_num] = min(distances_true[condition_num], d_true)
     else:
         distances_true[condition_num] = d_true
 
     if condition_num in distances_false.keys():
-        distances_false[condition_num] = min(
-            distances_false[condition_num], d_false)
+        distances_false[condition_num] = min(distances_false[condition_num], d_false)
     else:
         distances_false[condition_num] = d_false
 
@@ -46,9 +45,41 @@ def get_false_distance(condition_num):
 
 def has_reached_condition(condition_num):
     global distances_true, distances_false
-    return condition_num in distances_true.keys() or condition_num in distances_false.keys()
+    return (
+        condition_num in distances_true.keys()
+        or condition_num in distances_false.keys()
+    )
 
 
 def evaluate_condition(condition_num, op, lhs, rhs):
-    # TODO: COMPLETAR
-    return False
+    d_true = 0
+    d_false = 0
+    if type(lhs) == str:
+        lhs = ord(lhs)
+    if type(rhs) == str:
+        rhs = ord(rhs)
+    if op == "Eq":
+        d_true = abs(lhs - rhs)
+        d_false = 0 if lhs != rhs else K
+    elif op == "Ne":
+        d_true = 0 if lhs != rhs else K
+        d_false = abs(lhs - rhs)
+    elif op == "Lt":
+        d_true = 0 if lhs < rhs else lhs - rhs + K
+        d_false = 0 if lhs >= rhs else rhs - lhs
+    elif op == "Gt":
+        d_true = 0 if lhs > rhs else rhs - lhs + K
+        d_false = 0 if lhs <= rhs else lhs - rhs
+    elif op == "Le":
+        d_true = 0 if lhs <= rhs else lhs - rhs
+        d_false = 0 if lhs > rhs else rhs - lhs + K
+    elif op == "Ge":
+        d_true = 0 if lhs >= rhs else rhs - lhs
+        d_false = 0 if lhs < rhs else lhs - rhs + K
+    elif op == "In":
+        d_true = min([abs(el - lhs) for el in rhs]) if rhs else sys.maxsize
+        d_false = 0 if lhs not in rhs else K
+    else:
+        raise NotImplementedError("Operator not supported")
+    update_maps(condition_num, d_true, d_false)
+    return d_true == 0
--- taller_4/CgiDecode/test/test_evaluate_condition.py
+++ taller_4/CgiDecode/test/test_evaluate_condition.py
@@ -1,12 +1,251 @@
 #!./venv/bin/python
+import sys
 import unittest
-from src.evaluate_condition import evaluate_condition
+from src.evaluate_condition import (
+    evaluate_condition,
+    get_false_distance,
+    get_true_distance,
+)
 
 
 class TestEvaluateCondition(unittest.TestCase):
-    def testExample(self):
-        # COMPLETAR
-        evaluate_condition(1, "Eq", 10, 20)
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(True, False)
+    def assertExpectedDistances(
+        self, condition_num, op, lhs, rhs, exp_true, exp_false, exp_return
+    ):
+        self.assertEqual(evaluate_condition(condition_num, op, lhs, rhs), exp_return)
+        self.assertEqual(get_false_distance(condition_num), exp_false)
+        self.assertEqual(get_true_distance(condition_num), exp_true)
+
+    def testEqualTrue(self):
+        self.assertExpectedDistances(
+            condition_num=1,
+            op="Eq",
+            lhs=20,
+            rhs=10,
+            exp_true=10,
+            exp_false=0,
+            exp_return=False,
+        )
+
+    def testEqualFalse(self):
+        self.assertExpectedDistances(
+            condition_num=2,
+            op="Eq",
+            lhs=20,
+            rhs=20,
+            exp_true=0,
+            exp_false=1,
+            exp_return=True,
+        )
+
+    def testNotEqualTrue(self):
+        self.assertExpectedDistances(
+            condition_num=3,
+            op="Ne",
+            lhs=20,
+            rhs=10,
+            exp_true=0,
+            exp_false=10,
+            exp_return=True,
+        )
+
+    def testNotEqualFalse(self):
+        self.assertExpectedDistances(
+            condition_num=4,
+            op="Ne",
+            lhs=20,
+            rhs=20,
+            exp_true=1,
+            exp_false=0,
+            exp_return=False,
+        )
+
+    def testLowerEqualTrue(self):
+        self.assertExpectedDistances(
+            condition_num=5,
+            op="Le",
+            lhs=10,
+            rhs=20,
+            exp_true=0,
+            exp_false=11,
+            exp_return=True,
+        )
+
+    def testLowerEqualFalse(self):
+        self.assertExpectedDistances(
+            condition_num=6,
+            op="Le",
+            lhs=20,
+            rhs=10,
+            exp_true=10,
+            exp_false=0,
+            exp_return=False,
+        )
+
+    def testLowerEqualEqual(self):
+        self.assertExpectedDistances(
+            condition_num=7,
+            op="Le",
+            lhs=20,
+            rhs=20,
+            exp_true=0,
+            exp_false=1,
+            exp_return=True,
+        )
+
+    def testLowerTrue(self):
+        self.assertExpectedDistances(
+            condition_num=8,
+            op="Lt",
+            lhs=10,
+            rhs=20,
+            exp_true=0,
+            exp_false=10,
+            exp_return=True,
+        )
+
+    def testLowerFalse(self):
+        self.assertExpectedDistances(
+            condition_num=9,
+            op="Lt",
+            lhs=20,
+            rhs=10,
+            exp_true=11,
+            exp_false=0,
+            exp_return=False,
+        )
+
+    def testLowerEqual(self):
+        self.assertExpectedDistances(
+            condition_num=10,
+            op="Lt",
+            lhs=20,
+            rhs=20,
+            exp_true=1,
+            exp_false=0,
+            exp_return=False,
+        )
+
+    def testInEmpty(self):
+        self.assertExpectedDistances(
+            condition_num=11,
+            op="In",
+            lhs=10,
+            rhs=[],
+            exp_true=sys.maxsize,
+            exp_false=0,
+            exp_return=False,
+        )
+
+    def testInFalse(self):
+        self.assertExpectedDistances(
+            condition_num=12,
+            op="In",
+            lhs=10,
+            rhs=[1, 2, 3],
+            exp_true=7,
+            exp_false=0,
+            exp_return=False,
+        )
+
+    def testInTrue(self):
+        self.assertExpectedDistances(
+            condition_num=13,
+            op="In",
+            lhs=10,
+            rhs=[10],
+            exp_true=0,
+            exp_false=1,
+            exp_return=True,
+        )
+
+    def testTrueDuplicated(self):
+        self.assertExpectedDistances(
+            condition_num=14,
+            op="In",
+            lhs=10,
+            rhs=[10, 10],
+            exp_true=0,
+            exp_false=1,
+            exp_return=True,
+        )
+
+    def testFalseNear(self):
+        self.assertExpectedDistances(
+            condition_num=15,
+            op="In",
+            lhs=13,
+            rhs=[11, 12, 18],
+            exp_true=1,
+            exp_false=0,
+            exp_return=False,
+        )
+    
+    def testGreaterFalse(self):
+        self.assertExpectedDistances(
+            condition_num=16,
+            op="Gt",
+            lhs=10,
+            rhs=20,
+            exp_true=11,
+            exp_false=0,
+            exp_return=False,
+        )
+    
+    def testGreaterTrue(self):
+        self.assertExpectedDistances(
+            condition_num=17,
+            op="Gt",
+            lhs=20,
+            rhs=10,
+            exp_true=0,
+            exp_false=10,
+            exp_return=True,
+        )
+    
+    def testGreaterEqualFalse(self):
+        self.assertExpectedDistances(
+            condition_num=18,
+            op="Ge",
+            lhs=10,
+            rhs=20,
+            exp_true=10,
+            exp_false=0,
+            exp_return=False,
+        )
+    
+    def testGreaterEqualTrue(self):
+        self.assertExpectedDistances(
+            condition_num=19,
+            op="Ge",
+            lhs=20,
+            rhs=10,
+            exp_true=0,
+            exp_false=11,
+            exp_return=True,
+        )
+    
+    def testGreaterEqualEqual(self):
+        self.assertExpectedDistances(
+            condition_num=20,
+            op="Ge",
+            lhs=20,
+            rhs=20,
+            exp_true=0,
+            exp_false=1,
+            exp_return=True,
+        )
+
+    def testCharEqualTrue(self):
+        self.assertExpectedDistances(
+            condition_num=21,
+            op="Eq",
+            lhs="A",
+            rhs="A",
+            exp_true=0,
+            exp_false=1,
+            exp_return=True,
+        )
+    
+    def testNotImplementedOperator(self):
+        self.assertRaises(NotImplementedError, evaluate_condition, 21, "XX", 10, 20)
