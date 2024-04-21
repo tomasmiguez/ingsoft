89cbe7166c8586f754bb97c58d1257138132d0b9
Tomas Miguez <tomasmiguez99@gmail.com>
2024-04-21 17:22:00 -0300

    feat(taller 4): solucion ejercicio 4

--- taller_4/CgiDecode/src/get_fitness_cgi_decode.py
+++ taller_4/CgiDecode/src/get_fitness_cgi_decode.py
@@ -1,4 +1,31 @@
+from src.cgi_decode_instrumented import cgi_decode_instrumented
+from src.evaluate_condition import clear_maps, get_false_distance, get_true_distance
+
 def get_fitness_cgi_decode(test_suite):
-    fitness = 0
-    # TODO: COMPLETAR
-    return 0
+    CONDS_NUM = 5
+
+    best_true_dists = [1 for _ in range(CONDS_NUM)]
+    best_false_dists = [1 for _ in range(CONDS_NUM)]
+
+    for test_case in test_suite:
+        try:
+            cgi_decode_instrumented(test_case)
+        finally:
+            for cond_num in range(CONDS_NUM):
+                true_dist = get_true_distance(cond_num)
+                if true_dist is not None:
+                    normalized_dist = true_dist / (true_dist + 1)
+                    if normalized_dist < best_true_dists[cond_num]:
+                        best_true_dists[cond_num] = normalized_dist
+
+                false_dist = get_false_distance(cond_num)
+                if false_dist is not None:
+                    normalized_dist = false_dist / (false_dist + 1)
+                    if normalized_dist < best_false_dists[cond_num]:
+                        best_false_dists[cond_num] = normalized_dist
+
+            clear_maps()
+
+            continue
+
+    return sum(best_true_dists) + sum(best_false_dists)
