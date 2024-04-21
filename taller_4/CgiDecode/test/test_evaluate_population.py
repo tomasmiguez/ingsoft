6f2055d2a66cc857328af16c6312e21a95c35edf
Tomas Miguez <tomasmiguez99@gmail.com>
2024-04-21 17:57:05 -0300

    feat(taller 4): revolver ejercicio 6
    Hubo que agregar un clear_maps al principio de `get_fitness_cgi_decode`
    se ve que si no se puede buggear todo. No se porq pusieron estado
    global la verdad, demasiado rancio.

--- taller_4/CgiDecode/test/test_evaluate_population.py
+++ taller_4/CgiDecode/test/test_evaluate_population.py
@@ -1,14 +1,22 @@
 #!./venv/bin/python
 import unittest
-from src.create_population import create_population
 from src.evaluate_population import evaluate_population
 
 
-# class TestEvaluatePopulation(unittest.TestCase):
-#     def testExample(self):
-#         # COMPLETAR
-#         population = create_population(5)
-#         evaluate_population(population)
-#         self.assertTrue(True)
-#         self.assertFalse(False)
-#         self.assertEqual(True, False)
+class TestEvaluatePopulation(unittest.TestCase):
+    def test(self):
+        expected_fitnesses = {
+            tuple(["%AA"]): 2.36,
+            tuple(["%AU"]): 3.53,
+            tuple([""]): 8.5,
+            tuple([" %"]): 5.36,
+            tuple([" %1"]): 5.52,
+            tuple(["+"]): 6.5,
+            tuple(["+ %1"]): 4.67,
+            tuple([" %1+"]): 2.94,
+            tuple(["%AA", ""]): 2.36
+        }
+
+        rounded_result = {k: round(v, 2) for k, v in evaluate_population(expected_fitnesses.keys()).items()}
+
+        self.assertDictEqual(rounded_result, expected_fitnesses)
