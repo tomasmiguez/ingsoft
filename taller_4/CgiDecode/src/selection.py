155719d976fa55e5122f7be72e31903f467e4217
tomasmiguez <53789223+tomasmiguez@users.noreply.github.com>
2024-04-23 18:38:45 -0300

    Ex 7 (#5)
    * chore: comment tests so full suite can be run
    
    * chore(decode instrumented): reindex from 0
    
    This way it is easier to work with arrays
    
    * feat(taller 4): gitignore
    
    * feat(taller 4): solucion ejercicio 4
    
    * feat(taller 4): revolver ejercicio 6
    
    Hubo que agregar un clear_maps al principio de `get_fitness_cgi_decode`
    se ve que si no se puede buggear todo. No se porq pusieron estado
    global la verdad, demasiado rancio.
    
    * feat(taller 4): ejercicio 7
    
    ---------
    
    Co-authored-by: mybelgo <mybelgo@gmail.com>

--- taller_4/CgiDecode/src/selection.py
+++ taller_4/CgiDecode/src/selection.py
@@ -1,6 +1,8 @@
-from random import choices
+import random
 
 def selection(fitness_by_individual, tournament_size):
-    winner = None
-    # TODO: COMPLETAR
+    participants = {k: fitness_by_individual[k] for k in random.sample(fitness_by_individual.keys(), tournament_size)}
+
+    winner = min(participants, key = participants.get)
+
     return winner
