03d0d8e7f4077ca9f34b3a4bc4fc8b5a6c5426d1
mybelgo <mybelgo@gmail.com>
2024-05-01 00:14:32 -0300

    feat(taller 5): ex1

--- taller_5/MagicFuzzer/src/mutation_utils.py
+++ taller_5/MagicFuzzer/src/mutation_utils.py
@@ -1,16 +1,27 @@
+from random import choice, randint
+from string import printable
+
+
 def insert_random_character(s: str) -> str:
     """Retorna s con un caracter random insertado en una posicion al azar"""
-    pass
+    position = randint(0, len(s))
+    return s[:position] + choice(printable) + s[position:]
 
 
 def delete_random_character(s: str) -> str:
     """Retorna s con un caracter random eliminado.
     Si la cadena esta vacia, no la modifica"""
-    pass
+    if len(s) == 0:
+        return s
+    position = randint(0, len(s) - 1)
+    return s[:position] + s[position + 1 :]
 
 
 def change_random_character(s: str) -> str:
     """Retorna s con un caracter modificado en una posicion al azar.
     El caracter a modificar es reemplazado por otro caracter random.
     Si la cadena esta vacia, no la modifica."""
-    pass
+    if len(s) == 0:
+        return s
+    position = randint(0, len(s) - 1)
+    return s[:position] + choice(printable) + s[position + 1 :]
--- taller_5/MagicFuzzer/test/test_mutation_utils.py
+++ taller_5/MagicFuzzer/test/test_mutation_utils.py
@@ -1,12 +1,28 @@
 #!./venv/bin/python
 import unittest
 from random import seed
-from src.mutation_utils import insert_random_character, delete_random_character, change_random_character
+from src.mutation_utils import (
+    insert_random_character,
+    delete_random_character,
+    change_random_character,
+)
 
 
 class TestMutationUtils(unittest.TestCase):
-    def testExample(self):
-        insert_random_character("Hello World")
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(True, True)
+    def setUp(self):
+        seed(1)
+
+    def testInsertRandomCharacter(self):
+        self.assertEqual(insert_random_character("Hello World"), "He+llo World")
+
+    def testDeleteRandomCharacter(self):
+        self.assertEqual(delete_random_character("Hello World"), "Helo World")
+
+    def testChangeRandomCharacter(self):
+        self.assertEqual(change_random_character("Hello World"), "He+lo World")
+
+    def testDeleteRandomCharacterEmptyString(self):
+        self.assertEqual(delete_random_character(""), "")
+
+    def testChangeRandomCharacterEmptyString(self):
+        self.assertEqual(change_random_character(""), "")
