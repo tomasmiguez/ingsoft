cbd9bc973129ab782661f7b6d2d9075242ae37ad
mybelgo <mybelgo@gmail.com>
2024-05-01 23:51:03 -0300

    feat(taller 5): ex4

--- taller_5/MagicFuzzer/src/magic_fuzzer.py
+++ taller_5/MagicFuzzer/src/magic_fuzzer.py
@@ -1,6 +1,14 @@
+from random import choice
 from typing import List, Set
 from fuzzingbook.Coverage import Location
 from fuzzingbook.MutationFuzzer import FunctionCoverageRunner
+from src.mutation_utils import (
+    insert_random_character,
+    delete_random_character,
+    change_random_character,
+)
+from src.roulette_input_selector import RouletteInputSelector
+from src.get_source_lines import get_source_lines
 
 
 class MagicFuzzer:
@@ -9,29 +17,17 @@ class MagicFuzzer:
     ) -> None:
         """Ejecuta inputs iniciales, almacenando la cobertura obtenida"""
         self.initial_inputs = initial_inputs
-        self.contributing_inputs = []
+        self.contributing_inputs = dict()
         self.covered_locations = set()
         self.function_to_call = function_to_call
         self.function_name_to_call = function_name_to_call
+        self.lines = get_source_lines(function_to_call) - 1
 
-        runner = FunctionCoverageRunner(self.function_to_call)
-
-        for input in initial_inputs:
-            runner.run(input)
-            previous_coverage = len(self.covered_locations)
-            self.covered_locations = self.covered_locations.union(
-                set(
-                    filter(
-                        lambda x: x[0] == self.function_name_to_call, runner.coverage()
-                    )
-                )
-            )
-            if len(self.covered_locations) > previous_coverage:
-                self.contributing_inputs.append(input)
+        self.feed_fuzzer(initial_inputs)
 
     def get_contributing_inputs(self) -> List[str]:
         """Retorna la lista de los inputs que aumentaron la cobertura en el orden que fueron ejecutados"""
-        return self.contributing_inputs
+        return list(self.contributing_inputs.keys())
 
     def get_covered_locations(self) -> Set[Location]:
         """Retorna el conjunto de locaciones cubiertas de todas las ejecuciones"""
@@ -39,21 +35,55 @@ class MagicFuzzer:
 
     def mutate(self, s: str) -> str:
         """Aplica al azar alguna de las tres operaciones de mutacion definidas en el archivo mutation_utils.py"""
-        pass
+        possible_mutations = [
+            insert_random_character,
+            delete_random_character,
+            change_random_character,
+        ]
+        return choice(possible_mutations)(s)
+
+    def feed_fuzzer(self, inputs: List[str]) -> None:
+        """
+        Agrega nuevos inputs al fuzzer y ejecuta los que aumentan la cobertura
+        """
+        runner = FunctionCoverageRunner(self.function_to_call)
+
+        for input in inputs:
+            runner.run(input)
+            previous_coverage = len(self.covered_locations)
+            new_covered_locations = set(
+                filter(lambda x: x[0] == self.function_name_to_call, runner.coverage())
+            )
+            self.covered_locations = self.covered_locations.union(new_covered_locations)
+            if len(self.covered_locations) > previous_coverage:
+                self.contributing_inputs[input] = new_covered_locations
 
     def fuzz(self):
         """
         Elije aleatoriamente un input s usando seleccion de ruleta sobre e(s),
         muta el input s utilizando la función mutate(s), y ejecuta el s mutado
         """
-        pass
+        selector = RouletteInputSelector(2)
+        for input, covered_locations in self.contributing_inputs.items():
+            selector.add_new_execution(input, covered_locations)
+
+        selected_input = selector.select()
+        mutated_input = self.mutate(selected_input)
+
+        self.feed_fuzzer([mutated_input])
 
     def run_until_covered(self) -> int:
         """
-        Corre una campania del MagicFuzzer hasta cubrir todas las lineas del programa.
+        Corre una campaña del MagicFuzzer hasta cubrir todas las lineas del programa.
         Retorna la cantidad de iteraciones realizadas.
         """
-        pass
+        iterations = 0
+
+        while len(self.covered_locations) < self.lines:
+            self.fuzz()
+            iterations += 1
+
+        return iterations
 
     def run_iterations(self, n: int) -> None:
         """
--- taller_5/MagicFuzzer/test/test_ejercicio_4.py
+++ taller_5/MagicFuzzer/test/test_ejercicio_4.py
@@ -1,9 +1,32 @@
-#!./venv/bin/python
+from random import seed
 import unittest
+from src.magic_fuzzer import MagicFuzzer
+from src.crashme import crashme
+from parameterized import parameterized
 
 
 class TestEjercicio4(unittest.TestCase):
 
-    def test(self):
-        # TOOD: COMPLETAR
-        self.assertEqual(True, True)
+    @parameterized.expand(
+        [
+            [12, ["", "b", "ba", "bad", "bad!"], 5, 4756],
+            [26, ["", "b", "ba", "bad", "bad!"], 5, 4590],
+            [37, ["", "b", "ba", "bad", "bad!"], 5, 7927],
+        ]
+    )
+    def test(
+        self,
+        seed_value,
+        expected_contributing_inputs,
+        expected_input_quantity,
+        expected_iterations_until_covered,
+    ):
+        seed(seed_value)
+        fuzzer = MagicFuzzer([""], crashme, function_name_to_call="crashme")
+        iterations_until_covered = fuzzer.run_until_covered()
+
+        self.assertListEqual(
+            fuzzer.get_contributing_inputs(), expected_contributing_inputs
+        )
+        self.assertEqual(len(fuzzer.get_contributing_inputs()), expected_input_quantity)
+        self.assertEqual(iterations_until_covered, expected_iterations_until_covered)
