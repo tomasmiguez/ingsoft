59c63eef7f4a100e55717f5d22103ac151fa29f3
mybelgo <mybelgo@gmail.com>
2024-05-10 00:12:13 -0300

    fix(taller 4): ej4 fitness

--- taller_4/CgiDecode/src/get_fitness_cgi_decode.py
+++ taller_4/CgiDecode/src/get_fitness_cgi_decode.py
@@ -16,29 +16,28 @@ def get_fitness_cgi_decode(test_suite, coverage=False):
     for test_case in test_suite:
         try:
             cgi_decode_instrumented(test_case)
-        finally:
-            for cond_num in range(CONDS_NUM):
-                true_dist = get_true_distance(cond_num)
-                if true_dist is not None:
-                    normalized_dist = true_dist / (true_dist + 1)
-                    if normalized_dist < best_true_dists[cond_num]:
-                        best_true_dists[cond_num] = normalized_dist
+        except Exception as _:
+            pass
 
-                false_dist = get_false_distance(cond_num)
-                if false_dist is not None:
-                    normalized_dist = false_dist / (false_dist + 1)
-                    if normalized_dist < best_false_dists[cond_num]:
-                        best_false_dists[cond_num] = normalized_dist
+    for cond_num in range(CONDS_NUM):
+        true_dist = get_true_distance(cond_num)
+        if true_dist is not None:
+            normalized_dist = true_dist / (true_dist + 1)
+            if normalized_dist < best_true_dists[cond_num]:
+                best_true_dists[cond_num] = normalized_dist
 
-                if coverage:
-                    if true_dist == 0:
-                        true_coverage[cond_num] = 1
-                    if false_dist == 0:
-                        false_coverage[cond_num] = 1
+        false_dist = get_false_distance(cond_num)
+        if false_dist is not None:
+            normalized_dist = false_dist / (false_dist + 1)
+            if normalized_dist < best_false_dists[cond_num]:
+                best_false_dists[cond_num] = normalized_dist
 
-            clear_maps()
+        if coverage:
+            if true_dist == 0:
+                true_coverage[cond_num] = 1
+            if false_dist == 0:
+                false_coverage[cond_num] = 1
 
-            continue
     if coverage:
         return (sum(true_coverage) + sum(false_coverage)) / (CONDS_NUM * 2)
     return sum(best_true_dists) + sum(best_false_dists)
