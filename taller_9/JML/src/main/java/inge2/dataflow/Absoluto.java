2b3256dbe1c4e8863f3e5ee7ec7cb044df24e1f6
mybelgo <mybelgo@gmail.com>
2024-06-19 00:30:55 -0300

    feat(taller 9): Absoluto

--- taller_9/JML/src/main/java/inge2/dataflow/Absoluto.java
+++ taller_9/JML/src/main/java/inge2/dataflow/Absoluto.java
@@ -5,7 +5,9 @@ public class Absoluto {
     // Calcula el valor absoluto de un número entero.
     // Si el número es negativo, devuelve el opuesto.
     //
-    //@ TODO: ESPECIFICAR
+    //@ requires n > Integer.MIN_VALUE;
+    //@ ensures \result == n | \result == -n;
+    //@ ensures Integer.MIN_VALUE < \result <= Integer.MAX_VALUE;
     public static int valorAbsoluto(int n) {
         if (n < 0) {
             return -n;
