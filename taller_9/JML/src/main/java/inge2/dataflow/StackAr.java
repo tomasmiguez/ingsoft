be87949e68a329bec563ebae31cd9fa1df11af03
Tomas Miguez <tomasmiguez99@gmail.com>
2024-06-20 23:02:13 -0300

    feat(taller 9): StackAr especificacion

--- taller_9/JML/src/main/java/inge2/dataflow/StackAr.java
+++ taller_9/JML/src/main/java/inge2/dataflow/StackAr.java
@@ -5,69 +5,75 @@ public class StackAr {
     /**
      * Capacidad por defecto de la pila.
      */
+    //@ spec_public
     private final static int DEFAULT_CAPACITY = 10;
 
     /**
      * Arreglo que contiene los elementos de la pila.
      */
+    //@ spec_public
     private final int[] elems;
 
     /**
      * Indice del tope de la pila.
      */
+    //@ spec_public
     private int top = -1;
 
-    //@ TODO: ESPECIFICAR
+    //@ public invariant top < elems.length;
+    //@ public invariant top >= -1;
+
+    //@ ensures elems.length == DEFAULT_CAPACITY;
     public StackAr() {
         this(DEFAULT_CAPACITY);
     }
 
-    //@ TODO: ESPECIFICAR
+    //@ requires capacity > 0;
+    //@ requires capacity < Integer.MAX_VALUE;
+    //@ ensures elems.length == capacity;
     public StackAr(int capacity) {
-        if (capacity < 0) {
-            throw new IllegalArgumentException();
-        }
         this.elems = new int[capacity];
     }
 
-    //@ TODO: ESPECIFICAR
+    //@ ensures \result <==> (size() == 0);
+    //@ pure
     public boolean isEmpty() {
         return size() == 0;
     }
 
-    //@ TODO: ESPECIFICAR
+    //@ ensures \result <==> (size() == elems.length);
+    //@ pure
     public boolean isFull() {
         return size() == elems.length;
     }
 
-    //@ TODO: ESPECIFICAR
+    //@ ensures \result == (top + 1);
+    //@ pure
     public int size() {
         return top + 1;
     }
 
-    //@ TODO: ESPECIFICAR
+    //@ requires top < elems.length - 1;
+    //@ ensures top == \old(top) + 1;
+    //@ ensures peek() == o;
     public void push(int o) {
-        if (isFull()) {
-            throw new IllegalStateException();
-        }
         this.top++;
         this.elems[top] = o;
     }
 
-    //@ TODO: ESPECIFICAR
+    //@ requires top >= 0;
+    //@ ensures top == \old(top) - 1;
+    //@ ensures \result == \old(peek());
     public int pop() {
-        if (isEmpty()) {
-            throw new IllegalStateException();
-        }
+        int topValue = this.peek();
         top--;
-        return this.peek();
+        return topValue;
     }
 
-    //@ TODO: ESPECIFICAR
+    //@ requires top >= 0;
+    //@ ensures \result == elems[top];
+    //@ pure
     public int peek() {
-        if (isEmpty()) {
-            throw new IllegalStateException();
-        }
         return this.elems[top];
     }
 }
